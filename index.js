1163b75e8fdaf368097e46c49f67f86238f280ed
aHasnat1997 <hasnat56703@gmail.com>
2023-05-01 23:27:01 +0600

    deploy in varcel

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ const express = require('express')
 const app = express()
 const port = 7000
 const cors = require('cors')
-const chef = require('./chef-api.json');
-const recipe = require('./recipe-api.json');
+const chefs = require('./chef-api.json');
+const recipes = require('./recipe-api.json');
 
 app.use(cors())
 
 
 app.get('/', (req, res) => {
-  res.send('chef recipe hunter server')
+  res.send('chef recipe hunter server');
 })
 
 app.get('/chef', (req, res) => {
-  res.send(chef)
+  res.send(chefs);
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const chef = chefs.find(chef => chef.chef_id === id || {});
+  console.log(id);
+  res.send(chef);
 })
 
 app.get('/recipe', (req, res) => {
-  res.send(recipe)
+  res.send(recipes);
+})
+
+app.get('/recipe/:id', (req, res) => {
+  const id = (req.params.id);
+  const recipe = recipes.find(recipe => recipe.recipe_id === id || {});
+  res.send(recipe);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
