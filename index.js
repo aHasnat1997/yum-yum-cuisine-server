578f74c6b4a87182de03e6f3e704e68c6778ed7d
aHasnat1997 <hasnat56703@gmail.com>
2023-05-01 21:21:27 +0600

    chef api add

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const port = 7000
 const cors = require('cors')
+const chef = require('./chef-api.json');
 
 app.use(cors())
 
 
 app.get('/', (req, res) => {
   res.send('chef recipe hunter server')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef)
 })
 
 app.listen(port, () => {
